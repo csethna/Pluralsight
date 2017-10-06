7f436d914ae8f47b2337eb4093745c6a039b52d8
Cyrus Sethna <csethna@gmail.com>
2017-10-06 17:14:34 -0500

    conditionals in javascript

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -15,10 +15,18 @@ console.log("resultsDiv is " + typeof(resultsDiv));
 var none;
 console.log("none is " + typeof(none));
 
-var aNumber = 0;
+var aNumber = 10;
 console.log("aNumber is " + typeof(aNumber));
 
 var trueFalse = true;
 console.log("trueFalse is " + typeof(trueFalse));
 
 //msgs = "this shouldn't work";
+
+if (!none) {
+  console.log("none is undefined");
+}
+
+if (aNumber === "10") {
+  console.log("10 is 10");
+}
